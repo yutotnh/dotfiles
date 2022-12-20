e5d4927fecdb0b7c767c020338f84d772d110f8a
yutotnh <57719497+yutotnh@users.noreply.github.com>
2022-12-20 23:21:02 +0900

    add: install.sh #1

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -eu
+
+# yumやaptだと管理者権限が必要なので、Homebrewをインストールする
+# 参考: https://docs.brew.sh/Installation#unattended-installation
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+brew bundle --no-lock
+
+# To install useful key bindings and fuzzy completion:
+$(brew --prefix)/opt/fzf/install --completion --key-bindings --update-rc
+
+# 本当はHomebrewでGitをインストールして、そこの`git-completion.bash`を使いたい
+# だけど、2022/12現在bash-completionとGit両方をbrewでインストールしようとしたらエラーが発生する
+# そのため、解決するまでは `git-completion.bash`を直接ダウンロードして利用する
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
