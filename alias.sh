538bfda6f6bb07ff76d118f94b912e64fc22f70a
yutotnh <57719497+yutotnh@users.noreply.github.com>
2022-12-22 02:50:51 +0900

    add: alias.sh & bashrc.sh #11

---
+++ alias.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+alias cd..='cd ..'
+alias cd='z'
+alias e='exa --long --all --icons --time-style iso --ignore-glob ".git"'
+alias et='exa --long --all --icons --time-style iso --sort newest --ignore-glob ".git"'
+alias exit='exit 0'
+alias icdiff='icdiff -U 1 -N -H'
+alias less='less -NMR'
+alias open='xdg-open'
+alias sl='ls'
+alias ssh='ssh -XC'
+alias start='xdg-open'
+alias view='vim -RM'
---
+++ bashrc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eu
+
+# Set PATH, MANPATH, etc., for Homebrew.
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+eval "$(starship init bash)"
+eval "$(zoxide init bash)"
+
+if type brew &>/dev/null; then
+    HOMEBREW_PREFIX="$(brew --prefix)"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+        source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    else
+        for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+            [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+        done
+    fi
+fi
+
+. git-completion.bash
+
+. alias.sh
